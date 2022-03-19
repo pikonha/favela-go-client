a87d33f38575adbb779fa5780d7c7a5d7a9e53d0
lucas picollo <lpicollo@blockdaemon.com>
2022-03-19 18:18:17 -0300

    add nft details

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,82 @@
+import { BigNumber } from "ethers";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+import { contractHash } from '../../config'
+import useTokenContract from "../../hooks/useTokenContract";
+import ipfs from "../../utils/ipfs";
+
+type NFT = {
+  id: Number,
+  name: String
+  description: String,
+  extra_links: String[],
+  hidden: Boolean,
+  image: String,
+  lat: Number,
+  lng: Number,
+}
+
+export default function Nft() {
+  const [nft, nftSet] = useState<NFT>()
+  const contract = useTokenContract(contractHash)
+
+  const {query} = useRouter()
+
+  useEffect(() => {
+    const fetchNft = async () => {
+      if(contract){
+        const nft = await ipfs.getNftById(contract, BigNumber.from(query.id as String))
+        nftSet(nft)
+      }
+    }
+    fetchNft()
+  }, [contract, query.id])
+
+  if (!nft || !contract) {
+    return <span>Loading...</span>
+  }
+
+  return (
+    <div className="flex flex-col">
+      <div className="min-h-64">
+        <div
+          className="h-64 w-64 mx-auto rounded-md bg-cover bg-top bg-no-repeat"
+          style={{
+            backgroundImage: `url(${nft.image})`
+          }}
+        />
+      </div>
+      <div className="min-h-64 w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
+        <div className="flex">
+          <h3 className="font-semibold text-lg leading-tight truncate mr-2">{nft.name}</h3>
+          <span>
+            #{nft.id}
+          </span>
+        </div>
+        <p className="text-sm text-gray-600 tracking-wide font-semibold mt-2">
+          {nft.description}
+        </p>
+      </div>
+      <div className="max-w-4/5 mx-auto flex">
+        <button className="shadow bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+          Voltar
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  }
+};
+
+export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
+  return {
+      paths: [],
+      fallback: 'blocking'
+  }
+}
--- utils/ipfs.ts
+++ utils/ipfs.ts
@@ -1,11 +1,13 @@
 import axios from 'axios'
+import { BigNumber } from 'ethers'
 
 import { ERC20 } from "../contracts/types"
 import { ipfsURL } from '../config'
+import { NFT } from '../components/NFTList'
 
 const getUrl = (nftIpfsUrl) => {
-  const [, ipfsId] = nftIpfsUrl.split('//')
-  return `${ipfsURL}${ipfsId}`
+  const [,ipfsId] = nftIpfsUrl.split('//')
+  return ipfsId
 }
 
 async function getNftsFromAccount(contract: ERC20, account: string) {
@@ -14,7 +16,7 @@ async function getNftsFromAccount(contract: ERC20, account: string) {
   }
   const ids = await contract.getAllNftsIdsByAddress(account)
   const ipfsUrls = await Promise.all(ids.map(id => contract.tokenURI(id)))
-  const nfts = await Promise.all(ipfsUrls.map(url => axios.get(getUrl(url))))
+  const nfts = await Promise.all(ipfsUrls.map(url => axios.get(`${ipfsURL}/${getUrl(url)}`)))
   return nfts.map((n, i) => ({
     url: n.data.image,
     name: n.data.name,
@@ -22,10 +24,20 @@ async function getNftsFromAccount(contract: ERC20, account: string) {
     lat: n.data.lat,
     lng: n.data.lng,
     id: ids[i]
+  }))
+}
+
+async function getNftById(contract: ERC20, id: BigNumber) {
+  if (!contract) {
+    return
   }
-  ))
+
+  const tokenUri = await contract.tokenURI(id)
+  const nftRes = await axios.get(`${ipfsURL}/${getUrl(tokenUri)}`)
+  return nftRes.data
 }
 
 export default {
-  getNftsFromAccount
+  getNftsFromAccount,
+  getNftById
 }
