8f4ef92d859970e97dc80616ecf69913eba68be8
Richard Bertozzo <rbertozzo@blockdaemon.com>
2022-03-20 09:54:31 -0300

    add button for change camera

--- components/QrContainerReader.tsx
+++ components/QrContainerReader.tsx
@@ -1,7 +1,13 @@
 import { QrReader } from 'react-qr-reader';
 
 import React, { useState } from "react"
+import { MdAutorenew } from "react-icons/md";
 import { ERC20 } from '../contracts/types'
+
+const cameraType = {
+    BACK: "environment",
+    FRONT: "user"
+}
 
 const QrContainer = ({ contract, address }: { contract: ERC20, address: string }) => {
     const [data, setData] = useState('No result');
@@ -9,6 +15,15 @@ const QrContainer = ({ contract, address }: { contract: ERC20, address: string }
     const [error, setError] = useState('');
     const [processing, setProcessing] = useState(false)
     const [showDialog, setDialog] = useState(false)
+    const [selectedCamera, setSelectedCamera] = useState(cameraType.BACK)
+
+    function changeCamera() {
+        if (selectedCamera == cameraType.BACK) {
+            setSelectedCamera(cameraType.FRONT)
+        } else {
+            setSelectedCamera(cameraType.BACK)
+        }
+    }
 
     async function handlerScan(result, error) {
         if (error) {
@@ -87,10 +102,19 @@ const QrContainer = ({ contract, address }: { contract: ERC20, address: string }
             )}
             {!showDialog && !processing && (
                 <div>
+                    <div className='flex justify-center itens-center'>
+                        <MdAutorenew
+                            size={36}
+                            color="#EBEBE4"
+                            onClick={changeCamera}
+                        />
+                    </div>
+
+
                     <QrReader
                         scanDelay={500}
                         onResult={handlerScan}
-                        constraints={{ facingMode: 'user' }}
+                        constraints={{ facingMode: selectedCamera }}
                         videoStyle={{
                             width: '100%',
                             align: 'center'
